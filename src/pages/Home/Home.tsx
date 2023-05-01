771e7687648e736f206cca4d491fd0c0ec57dddf
Kirill <k.matviychuck@gmail.com>
2023-05-01 16:18:49 +0300

    redesigned header and main pages

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -4,26 +4,8 @@ import ReactTypingEffect from 'react-typing-effect';
 
 export const Home = () => {
     return (
-        <div className="w-full h-[94vh] relative flex flex-col justify-center items-center">
-            <div className='absolute left-0 top-0 w-full h-full z-10 bg-gradient-to-b from-black/70 to-black/70'></div>
-            <img className='absolute left-0 top-0 w-full h-full z-0 object-cover' src={bgImage} alt="background" />
-            <div className='flex'>
-                <h1 className='relative text-gray-400 text-5xl font-extrabold tracking-wider mr-2.5 z-20'>KIRILL</h1>
-                <h1 className='relative text-white text-5xl font-extrabold tracking-wider ml-2.5 z-20'>MATVIICHUK</h1>
-            </div>
-            <p className='relative z-20 text-white mt-2.5'>
-                <ReactTypingEffect
-                    text={[
-                        "Hello there, I'm a front-end developer",
-                        'If you are interested in my skills, write to me, all information in contacts',
-                        'Have a nice day 😉'
-                    ]}
-                    eraseDelay={2000}
-                    typingDelay={1000}
-                    eraseSpeed={30}
-                    speed={100}
-                />
-            </p>
+        <div className="w-full h-[91.4vh] bg-gray-700 flex flex-col justify-center items-center">
+
         </div>
     )
 }
