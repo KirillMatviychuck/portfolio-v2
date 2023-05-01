8995ac5b54dd7ddc56a0aca6d2b4949680806a7b
Kirill <k.matviychuck@gmail.com>
2023-05-01 12:07:21 +0300

    added react-typing-effect package

--- src/common/components/Header/Navigation/Navigation.tsx
+++ src/common/components/Header/Navigation/Navigation.tsx
@@ -2,7 +2,7 @@ import { NavLink } from "react-router-dom"
 
 export const Navigation = () => {
     return (
-        <ul className="flex justify-between items-center w-3/4 font-medium">
+        <ul className="flex justify-between items-center w-1/3 font-medium">
             <li><NavLink to={'/'} className={({ isActive }) => isActive ? 'text-sky-400' : 'text-white hover:text-sky-400'}>Home</NavLink></li>
             <li><NavLink to={'/about'} className={({ isActive }) => isActive ? 'text-sky-400' : 'text-white hover:text-sky-400'}>About Me</NavLink></li>
             <li><NavLink to={'/resume'} className={({ isActive }) => isActive ? 'text-sky-400' : 'text-white hover:text-sky-400'}>Resume</NavLink></li>
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,14 +1,29 @@
 import bgImage from '../../assets/images/main-background/main-background.jpg'
+// @ts-ignoreya
+import ReactTypingEffect from 'react-typing-effect';
 
 export const Home = () => {
     return (
-        <div className="w-full h-[94vh] relative flex justify-center items-center">
-            <div className='absolute left-0 top-0 w-full h-full z-10 bg-gradient-to-b from-black/10 to-black/70'></div>
+        <div className="w-full h-[94vh] relative flex flex-col justify-center items-center">
+            <div className='absolute left-0 top-0 w-full h-full z-10 bg-gradient-to-b from-black/70 to-black/70'></div>
             <img className='absolute left-0 top-0 w-full h-full z-0 object-cover' src={bgImage} alt="background" />
             <div className='flex'>
-                <h1 className='relative text-gray-400 text-5xl font-bold z-20'>KIRILL</h1>
-                <h1 className='relative text-white text-5xl z-20'>Matviichuk</h1>
+                <h1 className='relative text-gray-400 text-5xl font-extrabold tracking-wider mr-2.5 z-20'>KIRILL</h1>
+                <h1 className='relative text-white text-5xl font-extrabold tracking-wider ml-2.5 z-20'>MATVIICHUK</h1>
             </div>
+            <p className='relative z-20 text-white mt-2.5'>
+                <ReactTypingEffect
+                    text={[
+                        "Hello there, I'm a front-end developer",
+                        'If you are interested in my skills, write to me, all information in contacts',
+                        'Have a nice day 😉'
+                    ]}
+                    eraseDelay={2000}
+                    typingDelay={1000}
+                    eraseSpeed={30}
+                    speed={100}
+                />
+            </p>
         </div>
     )
 }
