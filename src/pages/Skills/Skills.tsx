43291dfd97d13f13d36e2d7c68e3e501d5db2ad9
Kirill <k.matviychuck@gmail.com>
2023-05-05 15:09:12 +0300

    added reveal effects to Skills component

--- src/pages/Skills/Skills.tsx
+++ src/pages/Skills/Skills.tsx
@@ -1,5 +1,8 @@
 import { Skill } from "../../common/components/Skill/Skill"
 
+const Flip = require('react-reveal/Flip')
+
+
 const skillsStore = [
     { title: "Development", description: "Creating single page applications using the React library, and using Redux, Redux-Toolkit as a state manager. Using GitHub for version control", svgPath: "m7.375 16.781 1.25-1.562L4.601 12l4.024-3.219-1.25-1.562-5 4a1 1 0 0 0 0 1.562l5 4zm9.25-9.562-1.25 1.562L19.399 12l-4.024 3.219 1.25 1.562 5-4a1 1 0 0 0 0-1.562l-5-4zm-1.649-4.003-4 18-1.953-.434 4-18z" },
     { title: "Foundation", description: "I have excellent knowledge of Javascript. Also, most of my SPA are written in Typescript. I have excellent knowledge in HTML, CSS, SCSS.", svgPath: "M19 2.01H6c-1.206 0-3 .799-3 3v14c0 2.201 1.794 3 3 3h15v-2H6.012C5.55 19.998 5 19.815 5 19.01c0-.101.009-.191.024-.273.112-.575.583-.717.987-.727H20c.018 0 .031-.009.049-.01H21V4.01c0-1.103-.897-2-2-2zm0 14H5v-11c0-.806.55-.988 1-1h7v7l2-1 2 1v-7h2v12z" },
@@ -11,10 +14,15 @@ export const Skills = () => {
     return (
         <div className="h-[91.4vh] w-[70%] bg-gray-800 flex flex-col items-center self-center text-white">
             <div className="h-[80%] w-full flex flex-col items-center justify-around">
-                <h1 className="text-5xl font-semibold">My <span className="text-cyan-400">Skills</span></h1>
-                <div className="w-full flex justify-between justify-self-">
-                    {skillsStore.map(skill => <Skill description={skill.description} title={skill.title} svgPath={skill.svgPath} />)}
-                </div>
+                <Flip top>
+                    <h1 className="text-5xl font-semibold">My <span className="text-cyan-400">Skills</span></h1>
+                </Flip>
+                <Flip left>
+                    <div className="w-full flex justify-between justify-self-">
+                        {skillsStore.map(skill => <Skill description={skill.description} title={skill.title} svgPath={skill.svgPath} />)}
+                    </div>
+                </Flip>
+
             </div>
         </div>
     )
