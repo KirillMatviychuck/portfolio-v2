29f5ff8509faaeee7c6164e51bb2560a656275f5
Kirill <k.matviychuck@gmail.com>
2023-05-21 15:24:46 +0300

    added Contact component

---
+++ src/pages/Contacts/ContactForm/ContactForm.tsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import { useFormik } from 'formik';
+
+export const ContactForm = () => {
+    const formik = useFormik({
+        initialValues: {
+            fullName: '',
+            emailAddress: '',
+            mobileNum: '',
+            emailSubj: '',
+            message: '',
+        },
+        onSubmit: values => {
+            alert(JSON.stringify(values, null, 2));
+        },
+    });
+    return (
+        <form className="w-full flex flex-col justify-center items-center text-white" onSubmit={formik.handleSubmit}>
+            <div className="flex justify-between mb-[6px] w-1/2">
+                <input
+                    className="w-[49%] h-[55px] bg-gray-700 p-3 rounded-lg"
+                    placeholder='Full Name'
+                    id="fullName"
+                    name="fullName"
+                    type="text"
+                    onChange={formik.handleChange}
+                    value={formik.values.fullName}
+                />
+                <input
+                    className="w-[49%] h-[55px] bg-gray-700 p-3 rounded-lg"
+                    placeholder='Email Address'
+                    id="emailAddress"
+                    name="emailAddress"
+                    type="email"
+                    onChange={formik.handleChange}
+                    value={formik.values.emailAddress}
+                />
+            </div>
+            <div className="flex justify-between mb-[6px] mt-[6px] w-1/2">
+                <input
+                    className="w-[49%] h-[55px] bg-gray-700 p-3 rounded-lg"
+                    placeholder="Mobile Number"
+                    id="mobileNum"
+                    name="mobileNum"
+                    type="tel"
+                    onChange={formik.handleChange}
+                    value={formik.values.mobileNum}
+                />
+                <input
+                    className="w-[49%] h-[55px] bg-gray-700 p-3 rounded-lg"
+                    placeholder="Email Subjecct"
+                    id="emailSubj"
+                    name="emailSubj"
+                    type="text"
+                    onChange={formik.handleChange}
+                    value={formik.values.emailSubj}
+                />
+            </div>
+            <textarea
+                className="h-72 w-1/2 mb-[6px] mt-[6px] bg-gray-700 p-3 rounded-lg"
+                placeholder="Your Message"
+                id="message"
+                name="message"
+                onChange={formik.handleChange}
+                value={formik.values.message}
+            />
+            <div className="relative mt-[6px]">
+                <button className="flex justify-center items-center w-[170px] h-[45px] py-4 px-4 bg-cyan-400 rounded-full text-gray-800 font-bold tracking-wide hover:text-white relative z-10">Send Message</button>
+                <button className="flex justify-center items-center w-[170px] h-[45px] py-4 px-4 bg-cyan-400 rounded-full text-gray-800 font-bold tracking-wide hover:text-white absolute bottom-0 left-0 blur z-0">Send Message</button>
+            </div>
+        </form>
+    );
+};
--- src/pages/Contacts/Contacts.tsx
+++ src/pages/Contacts/Contacts.tsx
@@ -1,10 +1,12 @@
+import { ContactForm } from "./ContactForm/ContactForm"
+
 export const Contacts = () => {
     return (
-        <div className="h-[91.4vh] w-[70%] bg-gray-800 border flex justify-center items-center self-center text-white">
+        <div className="h-[91.4vh] w-[70%] bg-gray-800 flex justify-center items-center self-center text-white">
             <div className="h-[90%] w-full flex flex-col items-center">
-                <h1 className="text-5xl font-semibold mt-12 mb-20">Latest <span className="text-cyan-400">Projects</span></h1>
-                <div className="flex flex-wrap justify-between items-center">
-
+                <h1 className="text-5xl font-semibold mb-20">Contact <span className="text-cyan-400">Me</span></h1>
+                <div className="w-full h-full flex flex-col items-center">
+                    <ContactForm />
                 </div>
             </div>
         </div>
