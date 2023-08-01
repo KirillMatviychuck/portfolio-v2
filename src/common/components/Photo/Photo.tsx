7bba2b66f17d31feac92f2805108e2afb63ac549
Kirill <k.matviychuck@gmail.com>
2023-08-01 13:20:52 +0300

    change mediaQ photo size

--- src/common/components/Photo/Photo.tsx
+++ src/common/components/Photo/Photo.tsx
@@ -6,12 +6,12 @@ const Zoom = require('react-reveal/Zoom');
 export const Photo: FC<Props> = ({ animation }) => {
     return (
         <Zoom>
-            <div className="w-[410px] h-[410px] 2sm:w-[300px] 2sm:h-[300px]">
+            <div className="w-[410px] h-[410px] 2sm:w-[230px] 2sm:h-[230px]">
                 <div className={`flex justify-center items-center w-full h-full rounded-full relative ${animation && "animate-[wiggle_4s_ease-in-out_infinite]"}`}>
                     <span className="absolute w-full h-full rounded-full bg-cyan-400 blur-lg"></span>
                     <span className="absolute w-full h-full rounded-full bg-cyan-400"></span>
-                    <div className="after:content-[''] absolute w-[380px] h-[380px] bg-gray-800 rounded-full 2sm:w-[260px] 2sm:h-[260px]">
-                        <div className="relative w-[380px] h-[380px] rounded-full overflow-hidden 2sm:w-[260px] 2sm:h-[260px]">
+                    <div className="after:content-[''] absolute w-[380px] h-[380px] bg-gray-800 rounded-full 2sm:w-[190px] 2sm:h-[190px]">
+                        <div className="relative w-[380px] h-[380px] rounded-full overflow-hidden 2sm:w-[190px] 2sm:h-[190px]">
                             <img className="w-[350px] object-cover absolute top-[25px]" src={myPhoto} alt="me" />
                         </div>
                     </div>
