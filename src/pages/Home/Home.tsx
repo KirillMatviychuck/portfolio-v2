1d4d2360e96fc6056cd6ff7384fab5acad83ae4b
Kirill <k.matviychuck@gmail.com>
2023-08-01 13:35:30 +0300

    change mediaQ Description.tsx

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,7 +3,7 @@ import { Photo } from "../../common/components/Photo/Photo"
 
 export const Home = () => {
     return (
-        <div className="h-[91.4vh] w-[70%] bg-gray-800 flex 2xl:flex-row justify-between items-center self-center 2sm:flex-col 2sm:justify-around">
+        <div className="h-[91.4vh] w-[70%] bg-gray-800 flex 2xl:flex-row justify-between items-center self-center 2sm:flex-col 2sm:justify-center">
             <Description />
             <Photo animation />
         </div>
