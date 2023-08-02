8bb14708b6f006bf5e403f4f0a9465d7329579c3
Kirill <k.matviychuck@gmail.com>
2023-08-02 19:07:28 +0300

    change min-height in Home.tsx

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,7 +3,7 @@ import { Photo } from "../../common/components/Photo/Photo"
 
 export const Home = () => {
     return (
-        <div className="h-[92.3vh] w-[70%] bg-gray-800 flex justify-between items-center self-center 2sm:flex-col 2sm:justify-around 2sm:h-screen">
+        <div className="h-[91.4vh] w-[70%] bg-gray-800 flex justify-between items-center self-center 2sm:flex-col 2sm:justify-around 2sm:min-h-screen">
             <Description />
             <Photo animation />
         </div>
